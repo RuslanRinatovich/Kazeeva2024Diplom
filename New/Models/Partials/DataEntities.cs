0fa20023635add919ff25747014033ce508bcd1b
RuslanRinatovich <safiullin@yandexlyceum.ru>
2024-04-25 12:21:58 +0300

    Оформить

---
+++ New/Models/Partials/DataEntities.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAssortmentCheck.Models
+{
+    public partial class DataEntities : DbContext
+    {
+        private static DataEntities _context;
+
+        public static DataEntities GetContext()
+        {
+            if (_context == null)
+            {
+                _context = new DataEntities();
+            }
+            return _context;
+        }
+    }
+}
---
+++ New/Pages/StatusPage.xaml.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfAssortmentCheck.Models;
+using WpfAssortmentCheck.Windows;
+
+
+namespace WpfAssortmentCheck.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для StatusPage.xaml
+    /// </summary>
+    public partial class StatusPage : Page
+    {
+        List<Status> brands;
+        public StatusPage()
+        {
+            InitializeComponent();
+        }
+
+
+        void LoadData()
+        {
+            try
+            {
+                DtData.ItemsSource = null;
+                //загрузка обновленных данных
+                DataEntities.GetContext().ChangeTracker.Entries().ToList().ForEach(p => p.Reload());
+                brands = DataEntities.GetContext().Status.OrderBy(p => p.Name).ToList();
+                DtData.ItemsSource = brands;
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка");
+            }
+        }
+        private void Page_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            //событие отображения данного Page
+            // обновляем данные каждый раз когда активируется этот Page
+            if (Visibility == Visibility.Visible)
+            {
+                LoadData();
+            }
+        }
+
+        private void DataGridGoodLoadingRow(object sender, DataGridRowEventArgs e)
+        {
+            e.Row.Header = (e.Row.GetIndex() + 1).ToString();
+        }
+
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+
+
+                StatusWindow window = new StatusWindow(new Status());
+                if (window.ShowDialog() == true)
+                {
+                    DataEntities.GetContext().Status.Add(window.currentItem);
+                    DataEntities.GetContext().SaveChanges();
+                    LoadData();
+                    MessageBox.Show("Запись добавлена", "Внимание", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка");
+            }
+        }
+
+        private void btnChange_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                // если ни одного объекта не выделено, выходим
+                if (DtData.SelectedItem == null) return;
+                // получаем выделенный объект
+                Status selected = DtData.SelectedItem as Status;
+
+
+                StatusWindow window = new StatusWindow(
+                    new Status
+                    {
+                        Id = selected.Id,
+                        Name = selected.Name,
+                        Color = selected.Color,
+                    }
+                    );
+
+                if (window.ShowDialog() == true)
+                {
+                    selected = DataEntities.GetContext().Status.Find(window.currentItem.Id);
+                    // получаем измененный объект
+                    if (selected != null)
+                    {
+
+                        selected.Id = window.currentItem.Id;
+                        selected.Name = window.currentItem.Name;
+                        selected.Color = window.currentItem.Color;
+                        DataEntities.GetContext().Entry(selected).State = EntityState.Modified;
+                        DataEntities.GetContext().SaveChanges();
+                        LoadData();
+                        MessageBox.Show("Запись изменена", "Внимание", MessageBoxButton.OK, MessageBoxImage.Information);
+                    }
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка");
+            }
+
+        }
+
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                // если ни одного объекта не выделено, выходим
+                if (DtData.SelectedItem == null) return;
+                // получаем выделенный объект
+                MessageBoxResult messageBoxResult = MessageBox.Show($"Удалить запись? ", "Удаление", MessageBoxButton.OKCancel,
+MessageBoxImage.Question);
+                if (messageBoxResult == MessageBoxResult.OK)
+                {
+                    Status deletedItem = DtData.SelectedItem as Status;
+
+
+                    DataEntities.GetContext().Buys.Load();
+                    var list = DataEntities.GetContext().Buys.Local;
+                    int k = 0;
+                    foreach (Buy item in list)
+                    {
+                        if (item.StatusId == deletedItem.Id)
+                            k++;
+                    }
+                    // MessageBox.Show(k.ToString());
+                    if (k > 0)
+                    {
+                        MessageBox.Show("Ошибка удаления, есть связанные записи", "Error",
+                            MessageBoxButton.OK, MessageBoxImage.Error);
+                        return;
+                    }
+                    DataEntities.GetContext().Status.Remove(deletedItem);
+                    DataEntities.GetContext().SaveChanges();
+                    MessageBox.Show("Запись удалена", "Внимание", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка, есть связанные записи");
+            }
+            finally
+            {
+                LoadData();
+            }
+        }
+    }
+}
